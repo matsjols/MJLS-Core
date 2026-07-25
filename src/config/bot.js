a1ad031c4dce6a3a258c737816d61dd16cd93334
matsjols <matsjols@gmail.com>
2026-07-25 02:57:59 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "ser over **matsjols**...",     // this is what people actually see
+        state: "ser over serveren...",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
